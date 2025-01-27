ae0dea210989df2d7fa386a8788d507bfe234592
Pramila Kolhe <nkolhepramila@gmail.com>
2025-01-27 16:18:32 +0530

    Prepare for deployment 11

--- client/src/services/ApiEndpoint.js
+++ client/src/services/ApiEndpoint.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 const instance=axios.create({
-    baseURL:'http://localhost:4000',
+    baseURL:'https://backend-stlp.onrender.com',
     headers:{
         'Content-Type':'application/json'
     },
