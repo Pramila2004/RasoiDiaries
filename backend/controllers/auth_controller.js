515f512ab455a02bcac57fd2894d986564276b20
Pramila Kolhe <nkolhepramila@gmail.com>
2025-01-27 16:12:52 +0530

    Prepare for deployment 10

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -78,7 +78,7 @@ export const logout = async (req, res) => {
       res.clearCookie("token", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "None",
       });
   
       // Send success response
